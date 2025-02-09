853a9beab08d2b9ca5d4702b816a3fc749cce154
Artem Lemur Stytsko <taifyn932802@gmail.com>
2025-02-09 19:30:36 +0300

    Revert "Added a function that takes as input a string with a date in the format"
    This reverts commit 85e4f7259c898eb76db5bb5d3d6e99a17e3fdd4e.
    Added function that converts date

--- src/widget.py
+++ src/widget.py
@@ -8,7 +8,7 @@ def mask_account_card(card_or_account: str) -> str:
     card_or_account_list = card_or_account.split()
     if "Счет" in card_or_account_list:
         return f"Счет {get_mask_account(card_or_account_list[1])}"
-    elif "MasterCard" in card_or_account_list or "Maestro" in card_or_account_list:
+    elif "MasterCard" or "Maestro" in card_or_account_list:
         return f"{card_or_account_list[0]} {get_mask_card_number(card_or_account_list[1])}"
     elif "Visa" in card_or_account_list:
         for i in card_or_account_list:
@@ -20,16 +20,5 @@ def mask_account_card(card_or_account: str) -> str:
         return f"{name_card[0]} {name_card[1]} {get_mask_card_number(str_numbers_card)}"
 
 
-def get_date(my_date: str) -> str:
-    date_obj = datetime.strptime(my_date, "%Y-%m-%dT%H:%M:%S.%f")
-    return date_obj.strftime("%d.%m.%Y")
 
-print(mask_account_card("Maestro 1596837868705199"))
-print(mask_account_card("Счет 64686473678894779589"))
-print(mask_account_card("MasterCard 7158300734726758"))
-print(mask_account_card("Счет 35383033474447895560"))
-print(mask_account_card("Visa Classic 6831982476737658"))
-print(mask_account_card("Visa Platinum 8990922113665229"))
-print(mask_account_card("Visa Gold 5999414228426353"))
-print(mask_account_card("Счет 73654108430135874305"))
-print(get_date("2024-03-11T02:26:18.671407"))
+
