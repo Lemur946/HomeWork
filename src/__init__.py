e79b1849e1ed993023f0b718ca18370f14af55f7
Artem Lemur Stytsko <taifyn932802@gmail.com>
2025-03-17 00:56:53 +0300

    All functions except testing are implemented

---
+++ src/__init__.py
@@ -0,0 +1,17 @@
+from utils import get_transactions_dictionary
+from external_api import convert_currency_to_rub
+from typing import List, Dict, Any
+
+# Loading transactions from JSON file
+transactions: List[Dict[str, Any]] = get_transactions_dictionary()
+
+# Convert each transaction and output the result
+for transaction in transactions:
+    if 'description' in transaction:
+        # Convert the transaction amount into rubles
+        converted_amount = convert_currency_to_rub(transaction)
+        # Output the conversion result
+        print(f"Converted amount for '{transaction['description']}': {converted_amount} RUB")  # Вывод результата
+    else:
+        # If the transaction description is missing, we display an error
+        print("Error: Transaction does not contain a description.")
---
+++ src/utils.py
@@ -0,0 +1,17 @@
+import json
+
+from typing import Any, List, Dict
+
+
+def get_transactions_dictionary() -> List[Dict[str, Any]]:
+    """A function that loads transactions from a JSON file and returns them as a dictionary."""
+    file_path = '../data/operations.json'  # Path to the file with transactions
+    try:
+        # Opening the file and loading JSON data
+        with open(file_path, "r", encoding="utf-8") as operations:
+            transactions = json.load(operations)
+            return transactions  # Return the list of transactions
+
+    except (json.JSONDecodeError, FileNotFoundError):
+        # In case of error, return an empty list
+        return []
